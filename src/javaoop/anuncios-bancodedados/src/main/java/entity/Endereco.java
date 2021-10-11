39d2814ad26c02f37294596564abcbe47d80bdc5
Patricia Souza <patricia.asouza@mercadolivre.com>
2021-10-11 12:59:44 -0300

    alteraçoes de classes Endereco e Forma de Pagamento

---
+++ src/javaoop/anuncios-bancodedados/src/main/java/entity/Endereco.java
@@ -0,0 +1,48 @@
+package entity;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "codigo_vendedor")
+    private Vendedor vendedor;
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private int numero;
+    private String uf;
+    private String cidade;
+
+    public Endereco(){}
+
+    public Endereco(String cep, String logradouro, String complemento, int numero, String uf, String cidade, Vendedor vendedor) {
+        super();
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.complemento = complemento;
+        this.numero = numero;
+        this.uf = uf;
+        this.cidade = cidade;
+        this.vendedor = vendedor;
+    }
+
+    public Endereco(Long id, String cep, String logradouro, String complemento, int numero, String uf, String cidade) {
+        super();
+        this.id = id;
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.complemento = complemento;
+        this.numero = numero;
+        this.uf = uf;
+        this.cidade = cidade;
+        this.vendedor = vendedor;
+    }
+}
---
+++ src/javaoop/anuncios-bancodedados/src/main/java/entity/FormaPagamento.java
@@ -0,0 +1,7 @@
+package entity;
+
+public enum FormaPagamento {
+
+    A_VISTA, //0
+    A_PRAZO //1
+}
